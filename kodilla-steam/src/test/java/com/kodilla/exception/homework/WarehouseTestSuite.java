470075de47c8a5098d030295e070188c3e47a8e7
Marcin Haczyk <haczyk.marcin@gmail.com>
2020-06-08 20:00:32 +0200

    Module_6.5_throwing_exception_changing

--- kodilla-steam/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
+++ kodilla-steam/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class WarehouseTestSuite {
 
     @Test(expected = OrderDoesntExistException.class)
-    public void testGetOrder_withException() throws OrderDoesntExistException {
+    public void testGetOrder_withException() throws Exception {
         Warehouse warehouse = new Warehouse();
         warehouse.getOrder("Not existing order");
     }
