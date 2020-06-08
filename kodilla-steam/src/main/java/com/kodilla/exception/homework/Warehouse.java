df259bfaa33c824e74ba5d196726e0cb441d12b3
Marcin Haczyk <haczyk.marcin@gmail.com>
2020-06-08 19:53:56 +0200

    Module_6.4_code_simplification

--- kodilla-steam/src/main/java/com/kodilla/exception/homework/Warehouse.java
+++ kodilla-steam/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -2,7 +2,6 @@ package com.kodilla.exception.homework;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Warehouse {
     List<Order> orderList = new ArrayList<>();
@@ -12,18 +11,6 @@ public class Warehouse {
     }
 
     public Order getOrder(String number) throws Exception {
-/*        List<Order> order = orderList
-                .stream()
-                .filter(o -> o.getNumber().contains(number))
-                .collect(Collectors.toList());
-        if (!order.isEmpty()) {                                                         // in the List should be only one object (if order number is on the list)
-            if (order.get(0).getNumber().equals(number)) {                              // protection in case if 0 index will be something different (bug)
-                return order.get(0);
-            }
-        }
-        throw new OrderDoesntExistException();*/
-        //return orderList.stream().filter(o -> o.getNumber().contains(number)).findFirst().orElseThrow(()-> new OrderDoesntExistException());
-        //return orderList.stream().filter(o -> o.getNumber().contains(number)).findFirst().orElseThrow(OrderDoesntExistException::new);
-        return orderList.stream().filter(o -> o.getNumber().contains(number)).findFirst().orElseThrow(()-> new Exception("To jest mój błąd"));
+        return orderList.stream().filter(o -> o.getNumber().contains(number)).findFirst().orElseThrow(OrderDoesntExistException::new);
     }
 }
